6be50184450f3a2feb7caf3b6600ee1b10832d33
danielwells <danielwells.usnret@gmail.com>
2014-03-18 17:42:08 -0500

    Initial file structure generated by 'rails new ...' command.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsProjblog::Application.config.secret_token = '9b9a3d48f89d3df5c307dbda70123ceebf3d2e3e14dbb542bde7a32f0e0a61accef58c62c2417bc49274ad8a4429244c9c1c9befa8c1fc0fd48e0d279b93a351'
