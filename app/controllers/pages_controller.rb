376123e45dd87eb55e5ffaa9d9ccdc1f487bf985
danielwells <danielwells.usnret@gmail.com>
2014-03-18 18:06:03 -0500

    Set up route and controller for bloghome page.

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,7 @@
+class PagesController < ApplicationController
+  
+  def bloghome
+    @test = "@test (from controller)"
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 RailsProjblog::Application.routes.draw do
+  
+  get "bloghome" => 'pages#bloghome'
+  
+  
+  
+  
+end
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -55,4 +64,3 @@ RailsProjblog::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
-end
